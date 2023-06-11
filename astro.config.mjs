855b3c1baf884d4c927629a504f86b8f0a39ed57
razvanandrei_ <68940868+razvanandreib@users.noreply.github.com>
2023-06-11 20:32:18 +0300

    added scss and some styling to the navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -24,32 +24,12 @@ function Search({ searchList }) {
   }
 
   return (
-    <div>
+    <main className="searchbar">
       <label
         htmlFor="search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-      >
-        Search
-      </label>
+      ></label>
       <div className="relative">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-search"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <circle cx={10} cy={10} r={7}></circle>
-            <line x1={21} y1={21} x2={15} y2={15}></line>
-          </svg>
-        </div>
         <input
           type="text"
           id="search"
@@ -63,18 +43,18 @@ function Search({ searchList }) {
                                focus:outline-none
                                focus:ring-blue-500
                                focus:border-blue-500"
-          placeholder="Search for anything..."
+          placeholder="Search for a coding term..."
         />
       </div>
 
       {query.length > 1 && (
-        <div className="my-4">
+        <div className="found">
           Found {posts.length} {posts.length === 1 ? "result" : "results"} for '
           {query}'
         </div>
       )}
 
-      <ul className="list-none">
+      <ul className="postfound">
         {posts &&
           posts.map((post) => (
             <li className="py-2">
@@ -90,7 +70,7 @@ function Search({ searchList }) {
             </li>
           ))}
       </ul>
-    </div>
+    </main>
   );
 }
 
