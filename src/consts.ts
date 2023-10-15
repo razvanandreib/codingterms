cda928f6e5e358ffb861b073e3555481e1398b29
razvanandreib <razvanandreibalauru@gmail.com>
2023-10-15 16:02:47 +0300

    form submit to test

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,9 @@
 export const SITE_TITLE = "Coding Terms";
 export const SITE_DESCRIPTION = "";
+
+export let logo = {
+  src: "/logo/codingtermslogo.png",
+  alt: "Coding Terms Logo",
+  width: 207,
+  height: 29,
+};
