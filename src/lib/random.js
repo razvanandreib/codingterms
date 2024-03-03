e79c925fe96d57fc7c19f6de1e98fa8e0274d634
razvanandreib <razvanandreibalauru@gmail.com>
2024-03-03 13:35:37 +0200

    random button del files

--- src/lib/random.js
+++ src/lib/random.js
@@ -1,4 +1,8 @@
-const randomButton(postData) {
+function randomButton(postData) {
     const min = 0;
     const max = postData.length;
+
+    const randomNumber = Math.floor(Math.random() * (max - min) + min)
+
+    window.location.href = postData[randomNumber].data.link
 }
